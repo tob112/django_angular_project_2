4bd06f9a5c216ed1b6288fa64e0058a65f378991
tobi <tolanger94@gmail.com>
2017-05-31 11:41:18 -0700

    first migration new userModel

--- django_angular_project_2/urls.py
+++ django_angular_project_2/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers
 
 
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^api/v1/', include(router.urls)),
+
 ]
